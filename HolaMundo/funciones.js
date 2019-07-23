17e87d5125b24271c997bf93bbc3f6c21d9ced73
Manuel's Mac <manuelsmac@Manuelss-MacBook-Pro.local>
2019-07-22 19:13:34 -0500

    Hola mundo con typescript

---
+++ HolaMundo/funciones.js
@@ -0,0 +1,14 @@
+"use strict";
+var saludo = "Hola Mundo desde Typescript";
+var numero;
+numero = 1;
+var cualquiera;
+cualquiera = 10;
+var PI = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(numero);
+    console.log(PI);
+}
+saludar();
---
+++ HolaMundo/funciones.ts
@@ -0,0 +1,18 @@
+let saludo:string = "Hola Mundo desde Typescript";
+
+let numero: number;
+numero = 1;
+
+let cualquiera: any;
+cualquiera = 10;
+
+const PI = 3.14;
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(numero);
+    console.log(PI);
+}
+
+saludar();
